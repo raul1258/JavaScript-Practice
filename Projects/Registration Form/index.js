e4411e9060b1e7fbffe016215accb49b7058390b
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-08-05 23:01:03 +0530

    new changes

--- Projects/Registration Form/index.js
+++ Projects/Registration Form/index.js
@@ -15,6 +15,7 @@ let stateInput = document.getElementById('state').value
 let zipInput = document.getElementById('zipcode').value
 let tnCInput = document.getElementById('tnC').checked
 
+    let error = false
     // console.log(firstNameInput,lastNameInput,emailInput,cityInput,stateInput,zipInput,tnCInput)
 
     if(firstNameInput.length >= 1){
@@ -23,6 +24,7 @@ let tnCInput = document.getElementById('tnC').checked
 	} else {
 		document.getElementById('first-name-invalid').style.display = 'block'
 		document.getElementById('first-name-valid').style.display = 'none'
+        error = true
 	}
 
     if(lastNameInput.length >=1){
@@ -31,6 +33,7 @@ let tnCInput = document.getElementById('tnC').checked
     }else{
         document.getElementById('last-name-invalid').style.display = 'block'
 		document.getElementById('last-name-valid').style.display = 'none'
+        error = true
     }
 
     /* Email
@@ -52,27 +55,64 @@ let tnCInput = document.getElementById('tnC').checked
     }else{
         document.getElementById('email-invalid').style.display = 'block'
         document.getElementById('email-valid').style.display = 'none'
+        error = true
     }
 
-    if(cityInput.length >=3){
+    if(cityInput.length >=3 && !parseInt(cityInput)){  //if(cityInput)
         document.getElementById('city-valid').style.display = 'block'
 		document.getElementById('city-invalid').style.display = 'none'
     }else{
         document.getElementById('city-invalid').style.display = 'block'
 		document.getElementById('city-valid').style.display = 'none'
+        error = true
     }
     
-    if(zipInput.length >=3){
+    if(zipInput.length === 6 && !isNaN(parseInt(zipInput))){  //important isNaN  (parseInt(zipInput)) we can use this
         document.getElementById('zip-valid').style.display = 'block'
 		document.getElementById('zip-invalid').style.display = 'none'
     }else{
         document.getElementById('zip-invalid').style.display = 'block'
 		document.getElementById('zip-valid').style.display = 'none'
+        error = true
     }
+
+    // console.log(parseInt(zipInput)) output nan
+    // console.log(isNaN(parseInt(zipInput))) output true
+    // NaN is falsy
+
+    if(stateInput !== 'None'){
+		document.getElementById('state-valid').style.display = "block";
+		document.getElementById('state-invalid').style.display = "none";
+	} else {
+		document.getElementById('state-invalid').style.display = "block";
+		document.getElementById('state-valid').style.display = "none";
+		error = true
+	}
 
     if(tnCInput == true){
         document.getElementById('valid-tnC').style.display = 'none'
     }else{
         document.getElementById('valid-tnC').style.display = 'block'
+        error = true
     }
+
+    if(!error) {
+		alert('Your details have been saved successfully!')
+		document.getElementById('first-name').value = ''
+		document.getElementById('last-name').value = ''
+		document.getElementById('email').value = ''
+		document.getElementById('city').value = ''
+		document.getElementById('state').value = 'None'
+		document.getElementById('zip').value = ''
+		document.getElementById('tnC').checked = false
+
+		document.getElementById('first-name-valid').style.display = 'none'
+		document.getElementById('last-name-valid').style.display = 'none'
+		document.getElementById("email-valid").style.display = "none"
+		document.getElementById("city-valid").style.display = "none";
+		document.getElementById("zip-valid").style.display = "none";
+		document.getElementById('state-valid').style.display = "none";
+	}
+
 }
+
