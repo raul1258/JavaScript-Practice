b85c4d8cc25c620fd5b13c550c65c9e116691d1f
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-08-06 23:08:36 +0530

    new project

--- Projects/Registration Form/index.js
+++ Projects/Registration Form/index.js
@@ -1,86 +1,66 @@
-// console.log('hi');
+function validate() {
+	let firstNameInput = document.getElementById('first-name').value
+	let lastNameInput = document.getElementById('last-name').value
+	let emailInput = document.getElementById('email').value
+	let cityInput = document.getElementById('city').value
+	let stateInput = document.getElementById('state').value
+	let zipInput = document.getElementById('zip').value
+	let tnCInput = document.getElementById('tnC').checked
 
-// function validate (){
-//     console.log('Submit button Clicked')
-// }
+	let error = false
 
-
-function validate(){
-    
-let firstNameInput = document.getElementById('first-name').value
-let lastNameInput = document.getElementById('last-name').value
-let emailInput = document.getElementById('email').value
-let cityInput = document.getElementById('city').value
-let stateInput = document.getElementById('state').value
-let zipInput = document.getElementById('zipcode').value
-let tnCInput = document.getElementById('tnC').checked
-
-    let error = false
-    // console.log(firstNameInput,lastNameInput,emailInput,cityInput,stateInput,zipInput,tnCInput)
-
-    if(firstNameInput.length >= 1){
+	if(firstNameInput){
 		document.getElementById('first-name-valid').style.display = 'block'
 		document.getElementById('first-name-invalid').style.display = 'none'
 	} else {
 		document.getElementById('first-name-invalid').style.display = 'block'
 		document.getElementById('first-name-valid').style.display = 'none'
-        error = true
+		error = true
 	}
 
-    if(lastNameInput.length >=1){
-        document.getElementById('last-name-valid').style.display = 'block'
+	if(lastNameInput){
+		document.getElementById('last-name-valid').style.display = 'block'
 		document.getElementById('last-name-invalid').style.display = 'none'
-    }else{
-        document.getElementById('last-name-invalid').style.display = 'block'
+	} else {
+		document.getElementById('last-name-invalid').style.display = 'block'
 		document.getElementById('last-name-valid').style.display = 'none'
-        error = true
-    }
+		error = true
+	}
 
-    /* Email
-	- Whether exists? - Done
-	- @ - Done
-	- . - Done
-	- After last ., there should be atleast 2 characters - Done
-	- First character cannot be @ - Done
-	*/
+	if (
+    emailInput &&
+    emailInput.includes("@") &&
+    emailInput.includes(".") &&
+    emailInput.lastIndexOf(".") <= emailInput.length - 3 &&
+		emailInput.indexOf('@') !== 0
+  ) {
+    document.getElementById("email-valid").style.display = "block";
+    document.getElementById("email-invalid").style.display = "none";
+  } else {
+    document.getElementById("email-invalid").style.display = "block";
+    document.getElementById("email-valid").style.display = "none";
+		error = true
+  }
 
-	// Eg: ankit@gmail.com
-	// Index of .: 11
-	// Length of string: 15
+	if(cityInput && cityInput.length >= 3) {
+		document.getElementById("city-valid").style.display = "block";
+		document.getElementById("city-invalid").style.display = "none";
+	} else {
+		document.getElementById("city-invalid").style.display = "block";
+		document.getElementById("city-valid").style.display = "none";
+		error = true
+	}
 
+	if(zipInput && zipInput.length === 6 && parseInt(zipInput)) {
+		document.getElementById("zip-valid").style.display = "block";
+		document.getElementById("zip-invalid").style.display = "none";
+	} else {
+		document.getElementById("zip-invalid").style.display = "block";
+		document.getElementById("zip-valid").style.display = "none";
+		error = true
+	}
 
-    if(emailInput.length >=1 && emailInput.includes('@') && emailInput.includes('.') && emailInput.lastIndexOf('.') <= emailInput.length-3 && emailInput.lastIndexOf('@') !==0){
-        document.getElementById('email-valid').style.display = 'block'
-        document.getElementById('email-invalid').style.display = 'none'
-    }else{
-        document.getElementById('email-invalid').style.display = 'block'
-        document.getElementById('email-valid').style.display = 'none'
-        error = true
-    }
-
-    if(cityInput.length >=3 && !parseInt(cityInput)){  //if(cityInput)
-        document.getElementById('city-valid').style.display = 'block'
-		document.getElementById('city-invalid').style.display = 'none'
-    }else{
-        document.getElementById('city-invalid').style.display = 'block'
-		document.getElementById('city-valid').style.display = 'none'
-        error = true
-    }
-    
-    if(zipInput.length === 6 && (parseInt(zipInput))){  //important isNaN  (parseInt(zipInput)) we can use this
-        document.getElementById('zip-valid').style.display = 'block'
-		document.getElementById('zip-invalid').style.display = 'none'
-    }else{
-        document.getElementById('zip-invalid').style.display = 'block'
-		document.getElementById('zip-valid').style.display = 'none'
-        error = true
-    }
-
-    // console.log(parseInt(zipInput)) output nan
-    // console.log(isNaN(parseInt(zipInput))) output true
-    // NaN is falsy
-
-    if(stateInput !== 'None'){
+	if(stateInput !== 'None'){
 		document.getElementById('state-valid').style.display = "block";
 		document.getElementById('state-invalid').style.display = "none";
 	} else {
@@ -89,14 +69,14 @@ let tnCInput = document.getElementById('tnC').checked
 		error = true
 	}
 
-    if(tnCInput == true){
-        document.getElementById('valid-tnC').style.display = 'none'
-    }else{
-        document.getElementById('valid-tnC').style.display = 'block'
-        error = true
-    }
-console.log(error)
-    if(!error) {
+	if(tnCInput) {
+		document.getElementById("tnC-invalid").style.display = "none";
+	} else {
+		document.getElementById("tnC-invalid").style.display = "block";
+		error = true
+	}
+
+	if(!error) {
 		alert('Your details have been saved successfully!')
 		document.getElementById('first-name').value = ''
 		document.getElementById('last-name').value = ''
@@ -113,6 +93,16 @@ console.log(error)
 		document.getElementById("zip-valid").style.display = "none";
 		document.getElementById('state-valid').style.display = "none";
 	}
-
 }
 
+	/* Email
+	- Whether exists? - Done
+	- @ - Done
+	- . - Done
+	- After last ., there should be atleast 2 characters - Done
+	- First character cannot be @ - Done
+	*/
+
+	// Eg: ankit@gmail.com
+	// Index of .: 11
+	// Length of string: 15
