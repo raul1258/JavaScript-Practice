afd2ed0d91e4cf23751a4622913f03ba320a8459
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-08-12 15:05:28 +0530

    new update

--- Projects/Authentication/index.js
+++ Projects/Authentication/index.js
@@ -42,14 +42,14 @@ const decrypt = (encryptedPassword) => {
   
   const DB_USERS = []
   
-  const resetSignupFields = () => {
-	  document.getElementById('signup-first-name').value = ''
-	  document.getElementById('signup-last-name').value = ''
-	  document.getElementById('signup-phone').value = ''
-	  document.getElementById('signup-email').value = ''
-	  document.getElementById('signup-password').value = ''
-	  document.getElementById('signup-confirm-password').value = ''
-  }
+//   const resetSignupFields = () => {
+// 	  document.getElementById('signup-first-name').value = ''
+// 	  document.getElementById('signup-last-name').value = ''
+// 	  document.getElementById('signup-phone').value = ''
+// 	  document.getElementById('signup-email').value = ''
+// 	  document.getElementById('signup-password').value = ''
+// 	  document.getElementById('signup-confirm-password').value = ''
+//   }
   
   const resetLoginFields = () => {
 	  document.getElementById('login-email').value = ''
@@ -65,10 +65,12 @@ const decrypt = (encryptedPassword) => {
 	  let confirmPassword = document.getElementById('signup-confirm-password').value
 	  let tnCInput = document.getElementById('tnC').checked
 
-	  
+	  let error = false
+
+	  console.log(password)
   
 	  let signupSuccessAlert = document.getElementById('signup-alert-success')
-	  // let signupFailureAlert = document.getElementById('signup-alert-failure') - Validation failure
+	 //let signupFailureAlert = document.getElementById('signup-alert-failure')  //Validation failure
 
 	  if(firstName){
 		document.getElementById('first-name-valid').style.display = "block"
@@ -76,7 +78,8 @@ const decrypt = (encryptedPassword) => {
 	  }else{
 		document.getElementById('first-name-valid').style.display = "none"
 		document.getElementById('first-name-invalid').style.display = "block"
-	  }
+		error = true
+	}
   
 	  if(lastName){
 		document.getElementById('last-name-valid').style.display = "block"
@@ -84,7 +87,8 @@ const decrypt = (encryptedPassword) => {
 	  }else{
 		document.getElementById('last-name-valid').style.display = "none"
 		document.getElementById('last-name-invalid').style.display = "block"
-	  }
+		error = true  
+	}
 	  
 	  if (email && email.includes("@") && email.includes(".") && email.lastIndexOf(".") <= email.length - 3 && email.indexOf('@') !== 0 ) {
 		document.getElementById("email-valid").style.display = "block";
@@ -92,7 +96,8 @@ const decrypt = (encryptedPassword) => {
 	  } else {
 		document.getElementById("email-invalid").style.display = "block";
 		document.getElementById("email-valid").style.display = "none";
-	  }
+		error = true  
+	}
 
 	  if(phone && phone.length === 10 && parseInt(phone)){
 		document.getElementById("phone-valid").style.display = "block";
@@ -100,7 +105,8 @@ const decrypt = (encryptedPassword) => {
 		} else {
 		document.getElementById("phone-invalid").style.display = "block";
 		document.getElementById("phone-valid").style.display = "none";
-		}
+		error = true	
+	}
 		
 		if(confirmPassword && password === confirmPassword){
 			document.getElementById('valid-confirmpassword').style.display = "block"
@@ -108,6 +114,7 @@ const decrypt = (encryptedPassword) => {
 		}else{
 			document.getElementById('valid-confirmpassword').style.display = "none"
 			document.getElementById('invalid-confirmpassword').style.display = "block"
+			error = true
 		}
 
 		if(tnCInput) {
@@ -117,6 +124,26 @@ const decrypt = (encryptedPassword) => {
 			error = true
 		}
 
+		if(!error) {
+			document.getElementById('signup-first-name').value = ''
+			document.getElementById('signup-last-name').value = ''
+			document.getElementById('signup-phone').value = ''
+			document.getElementById('signup-email').value = ''
+			document.getElementById('signup-password').value = ''
+			document.getElementById('signup-confirm-password').value = ''
+
+
+		document.getElementById('first-name-valid').style.display = 'none'
+		document.getElementById('last-name-valid').style.display = 'none'
+		document.getElementById("email-valid").style.display = "none"
+		document.getElementById("phone-valid").style.display = "none";
+		document.getElementById('valid-confirmpassword').style.display = "none"
+		document.getElementById("tnC-invalid").style.display = "none";
+		
+
+		signupSuccessAlert.style.display = 'block'
+	
+		}
 
 	  let userDetails = {
 		  firstName,
@@ -127,11 +154,11 @@ const decrypt = (encryptedPassword) => {
 	  }
   
 	  DB_USERS.push(userDetails)
-	  signupSuccessAlert.style.display = 'block'
+	  
   
 	  console.log(DB_USERS)
   
-	  resetSignupFields()
+	//   resetSignupFields()
 
 
   }
@@ -139,6 +166,7 @@ const decrypt = (encryptedPassword) => {
   const login = () => {
 	  let enteredEmail = document.getElementById('login-email').value
 	  let enteredPassword = document.getElementById('login-password').value
+	  console.log(enteredPassword)
   
 	  let loginSuccessAlert = document.getElementById('login-alert-success')
 	  let loginFailureAlert = document.getElementById('login-alert-failure')
@@ -167,6 +195,20 @@ const decrypt = (encryptedPassword) => {
 				icon.classList.remove("fa-eye-slash")
 			}
 	})
+
+	const toggle1 = document.getElementById('btnToggle1'),
+		confirmPassword = document.getElementById('signup-confirm-password'),
+		icon1 = document.getElementById('eyeIcon1');
+
+		toggle1.addEventListener("click", () => {
+			if(confirmPassword.type === 'password'){
+				confirmPassword.type = 'text';
+				icon1.classList.add("fa-eye-slash")
+			}else{
+				confirmPassword.type = 'password';
+				icon1.classList.remove("fa-eye-slash")
+			}
+		})
 
   /*
 		  find() -> Return a condition to find the element; If value exists, returns the value; else, returns undefined
