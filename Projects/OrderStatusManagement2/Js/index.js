0c0d4cf97725ce8bb5aa12a1b1b53991f1c5fba5
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-08-22 00:14:57 +0530

    DOM manipulation

---
+++ Projects/OrderStatusManagement2/Js/index.js
@@ -0,0 +1,21 @@
+const executeOrder = () => {
+	let orderId = document.getElementById('order-id-input').value
+	document.getElementById('order-id-input').value = ''
+
+	createOrderCard(orderId)
+
+	document.getElementById(orderId).innerText = 'OrderPlaced'
+
+	chefReceived(orderId)
+		.then(pizzaSauceAdded)
+		.then(firstLayerOfCheezeAdded)
+		.then(toppingsAdded)
+		.then(secondLayerOfCheezeAdded)
+		.then(pizzaBaked)
+		.then(oreganoAddedAndPacked)
+		.then(packageReceivedAtCounter)
+		.then(() => document.getElementById(orderId).innerText = 'Package received at counter')
+		.catch((err) => console.log(err))
+}
+
+
