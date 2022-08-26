256ff2f419de02aeba044b79a981891c43825de8
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-08-27 00:16:10 +0530

    updated version of pizza tracking

--- Projects/OrderStatusManagement2/Js/index.js
+++ Projects/OrderStatusManagement2/Js/index.js
@@ -1,12 +1,38 @@
 
+let currentOrderNumber = 1
+
 const executeOrder = () => {
-	
-	let orderId = document.getElementById('order-id-input').value
-	document.getElementById('order-id-input').value = ''
-	
-	if(orderId.length > 2){
-	createOrderCard(orderId)
-	document.getElementById(orderId).innerText = 'OrderPlaced'
+	let orderId = createOrderId()
+
+	let item1 = document.getElementById('item-1').value
+	let price1 = document.getElementById('price-1').value
+	let item2 = document.getElementById('item-2').value
+	let price2 = document.getElementById('price-2').value
+	let item3 = document.getElementById('item-3').value
+	let price3 = document.getElementById('price-3').value
+
+	resetFields()
+
+	let billDetails = [
+		{
+			item: item1,
+			price: Number(price1),
+		},
+		{
+			item: item2,
+			price: Number(price2),
+		},
+		{
+			item: item3,
+			price: Number(price3),
+		}
+	]
+
+	createOrderCard(orderId, billDetails)
+
+	currentOrderNumber++
+
+	document.getElementById(orderId).innerText = 'Order Placed'
 
 	chefReceived(orderId)
 		.then(pizzaSauceAdded)
@@ -16,11 +42,69 @@ const executeOrder = () => {
 		.then(pizzaBaked)
 		.then(oreganoAddedAndPacked)
 		.then(packageReceivedAtCounter)
-		.then(() => document.getElementById(orderId).innerText = 'Pizzza Delivered')
+		.then(() => document.getElementById(orderId).innerText = 'Package received at counter')
 		.catch((err) => console.log(err))
-}else{
-	alert("Please enter valid Order Id")
 }
+
+const resetFields = () => {
+	document.getElementById('item-1').value = ''
+	document.getElementById('price-1').value = ''
+	document.getElementById('item-2').value = ''
+	document.getElementById('price-2').value = ''
+	document.getElementById('item-3').value = ''
+	document.getElementById('price-3').value = ''
+} 
+
+const createOrderId = () => {
+	let todaysDate = new Date()
+
+	let temp = `MP${todaysDate.getFullYear()}${todaysDate.getMonth() + 1}${todaysDate.getDate()}`
+
+	let numOfPreceedingZeroes = 3
+	if(currentOrderNumber >= 10)
+		numOfPreceedingZeroes = 2
+	else if(currentOrderNumber >= 100)
+		numOfPreceedingZeroes = 1
+	else if(currentOrderNumber >= 1000)
+		numOfPreceedingZeroes = 0
+	
+	while(numOfPreceedingZeroes--)
+		temp = temp + '0'
+
+	return `${temp}${currentOrderNumber}`
 }
+
+
+
+// Previous Version
+
+// const executeOrder = () => {
+	
+// 	let orderId = document.getElementById('order-id-input').value
+// 	document.getElementById('order-id-input').value = ''
+
+// 	let item1 = document.getElementById('item-1').value
+// 	let item2 = document.getElementById('item-1').value
+// 	let item3 = document.getElementById('item-1').value
+
+	
+// 	if(orderId.length > 2){
+// 	createOrderCard(orderId)
+// 	document.getElementById(orderId).innerText = 'OrderPlaced'
+
+// 	chefReceived(orderId)
+// 		.then(pizzaSauceAdded)
+// 		.then(firstLayerOfCheezeAdded)
+// 		.then(toppingsAdded)
+// 		.then(secondLayerOfCheezeAdded)
+// 		.then(pizzaBaked)
+// 		.then(oreganoAddedAndPacked)
+// 		.then(packageReceivedAtCounter)
+// 		.then(() => document.getElementById(orderId).innerText = 'Pizzza Delivered')
+// 		.catch((err) => console.log(err))
+// }else{
+// 	alert("Please enter valid Order Id")
+// }
+// }
 
 
